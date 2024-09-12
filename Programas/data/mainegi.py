6b1501acb1b8006b68fb7ac8946a54a4cdf17fe2
Bryan Alejandro <102688465+BryGP@users.noreply.github.com>
2024-09-12 16:21:16 -0600

    Add files via upload

--- Programas/data/mainegi.py
+++ Programas/data/mainegi.py
@@ -1,28 +1,26 @@
 import pandas as pd
-import matplotlib.pyplot as plt
 import seaborn as sns
-import os
-import sys
-
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+import matplotlib.pyplot as plt
 
 # Cargar los datos del archivo CSV
-read_inegi_dataset = 'data/datos_inegi.csv'  # Cambia esto por la ruta a tu archivo
-data = pd.read_csv(read_inegi_dataset)
+file_path = r'C:\Users\bryan\Documents\ITQ\Semestre 8\Ciencia de Datos\DS env\Programas\data\datos_inegi.csv'  # Ruta absoluta
+data = pd.read_csv(file_path)
 
-# Mostrar las columas del archivo
+# Mostrar las primeras filas del dataframe para entender la estructura de los datos
 print(data.head())
 
-# Elegimos un estado de interés
-estado = '04'  # Cambia esto por el estado o región de interés
-data_estado = data[data['MUN_REGIS'] == estado]
+estado = '1'
+data_estado = data[data['mun_regis'] == estado]
 
-# Seleccionar tres variables de interés
-variables_interes = ['EDAD_MADR', 'ESCOL_MAD', 'HIJOS_VIVO']  # Cambia esto por las variables de interés
+# Seleccionar las columnas a mostrar
+variables_interes = ['edad_madn', 'ESCOL_MAD', 'SEXO']
 data_estado = data_estado[variables_interes]
 
-# Eliminar filas con datos faltantes en las variables de interés
+# Eliminamos los residuos
 data_estado.dropna(inplace=True)
+
+# Convertir las variables categóricas en variables dummy
+data_estado = pd.get_dummies(data_estado, columns=['ESCOL_MAD', 'SEXO'], drop_first=True)
 
 # Generar un heatmap de correlación entre las variables
 correlation_matrix = data_estado.corr()
@@ -32,9 +30,5 @@ plt.figure(figsize=(10, 8))
 
 # Generar el heatmap
 sns.heatmap(correlation_matrix, annot=True, cmap='coolwarm', linewidths=0.5)
-
-# Agregar título
 plt.title(f'Heatmap de Correlación de Variables en {estado}')
-
-# Mostrar el gráfico
 plt.show()
